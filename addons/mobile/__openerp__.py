80e189f03fa29651ea413f7fe30c869ff43ad49a
Diogo Duarte <diogo.duarte@vnc.biz>
2015-01-22 14:17:38 +0000

    first commit

---
+++ addons/mobile/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mobile Website Builder",
+
+    'summary': """
+        Implements mobile website for your phone""",
+    'description': """
+        Mobile Javascript application framework implementation
+    """,
+    'author': "Diogo Duarte",
+    'website': "http://diogocduarte.github.io",
+    'category': 'Mobile',
+    'version': '0.1',
+    'depends': ['base', 'hr_timesheet'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+}
